86b7d0e9f4e13a5804ad2da1e662b7501184f156
losbiw <skorinov41@gmail.com>
2022-04-24 04:15:15 +0100

    Add meta description

--- components/general/meta.tsx
+++ components/general/meta.tsx
@@ -1,6 +1,7 @@
 import useTranslation from "next-translate/useTranslation";
 import Head from "next/head";
 import React, { FC } from "react";
+import server from "@/constants/server";
 
 interface Props {
   title: string;
@@ -18,7 +19,7 @@ const MetaTags: FC<Props> = ({ children, title, description }) => {
         <meta name="description" content={description || t("description")} />
         <meta name="og:title" content={title} />
         <meta name="og:description" content={description || t("description")} />
-        <meta name="og:image" content="https://take-in-ukrainians.com/og.png" />
+        <meta name="og:image" content={`${server}/og.png`} />
       </Head>
 
       {children}
